f018c4140a9e76d787721ec2cae1f110970fb610
SASHMITA JENA <sashmitajena0498@gmail.com>
2023-04-12 17:20:56 +0530

    video-3-home-page

--- AMAZON/README.md
+++ AMAZON/README.md
@@ -1,8 +1,12 @@
 #MERN AMAZON
 
-
 #Lessons
 1.Introduction
 2.Install Tools
 3.Create React App
 4.Create Git Repository
+5.List Products
+1.create products array
+2.add products image
+3.render products
+4.style products
---
+++ AMAZON/frontend/src/data.js
@@ -0,0 +1,54 @@
+const data={
+    products:[
+        {
+            name:'Nike Slim Shirt',
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high-quality product'
+        },
+        {
+            name:'Adidas Slim Shirt',
+            slug:'adidas-slim-shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:125,
+            countInStock:15,
+            brand:'Adidas',
+            rating:4.3,
+            numReviews:15,
+            description:'high-quality product'
+        },
+        {
+            name:'Nike Slim pants',
+            slug:'nike-slim-pants',
+            category:'Pants',
+            image:'/images/p3.jpg',
+            price:30,
+            countInStock:20,
+            brand:'Nike',
+            rating:4.7,
+            numReviews:25,
+            description:'high-quality product'
+        },
+        {
+            name:'Adidas Slim Pants',
+            slug:'adidas-slim-pants',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:20,
+            countInStock:10,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'high-quality product'
+        },
+    ],
+};
+
+export default data;
